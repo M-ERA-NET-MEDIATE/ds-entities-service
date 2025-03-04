f0db155b98a4c320a05f64d07c8cef5d48b69664
Casper Welzel Andersen <43357585+CasperWA@users.noreply.github.com>
2025-03-04 14:49:36 +0100

    Use SOFT7IdentityURI and type from s7 (#112)
    * Use SOFT7IdentityURI and type from s7
    
    * Only use basic types in Mongo query

--- dataspaces_entities/models/__init__.py
+++ dataspaces_entities/models/__init__.py
@@ -2,12 +2,7 @@
 
 from __future__ import annotations
 
-import re
-
 from .auth import DSAPIRole
 from .service_errors import HTTPError
 
-URI_REGEX = re.compile(r"^(?P<namespace>https?://.+)/(?P<version>\d(?:\.\d+){0,2})/(?P<name>[^/#?]+)$")
-"""Regular expression to parse a SOFT entity URI."""
-
-__all__ = ("URI_REGEX", "DSAPIRole", "HTTPError")
+__all__ = ("DSAPIRole", "HTTPError")
